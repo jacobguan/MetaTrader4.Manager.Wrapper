0bc47cd24f13741af635b638ac6f00f5a262d74e
pantonis <antonischa@outlook.com>
2016-08-05 21:56:31 +0300

    Update to v.985

--- src/P23.MetaTrader4.Manager.ClrWrapper/PerformanceInfo.cpp
+++ src/P23.MetaTrader4.Manager.ClrWrapper/PerformanceInfo.cpp
@@ -4,7 +4,7 @@
 IList<P23::MetaTrader4::Manager::Contracts::PerformanceInfo^>^ P23::MetaTrader4::Manager::ClrWrapper::PerformaneRequest(UInt32 from)
 {
 	int total = 0;
-	PerformanceInfo* infos = _manager->Manager->PerformaneRequest(from, &total);
+	PerformanceInfo* infos = _manager->Manager->PerformanceRequest(from, &total);
 	IList<P23::MetaTrader4::Manager::Contracts::PerformanceInfo^>^ result = gcnew List<P23::MetaTrader4::Manager::Contracts::PerformanceInfo^>();
 	for (int i = 0; i < total; i++)
 		result->Add(Convert(&infos[i]));
